cee370d89dc64effe6dd1e787bfeecf6713c5a13
fw <thatgloomyafternoon@gmail.com>
2025-12-15 23:57:02 +0700

    progress

---
+++ src/main/java/com/fw/irongate/configurations/BeanConfig.java
@@ -0,0 +1,27 @@
+package com.fw.irongate.configurations;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+
+  @Value("${jwt.secret}")
+  private String secret;
+
+  @Bean
+  public JWTVerifier jwtVerifier() {
+    return JWT.require(Algorithm.HMAC256(secret)).build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
