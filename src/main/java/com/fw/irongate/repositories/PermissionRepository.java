98f46c51c3d61d60ab6c5801e10e07be7bfe232b
fw <thatgloomyafternoon@gmail.com>
2025-12-17 23:40:06 +0700

    progress

---
+++ src/main/java/com/fw/irongate/repositories/PermissionRepository.java
@@ -0,0 +1,22 @@
+package com.fw.irongate.repositories;
+
+import com.fw.irongate.models.entities.Permission;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PermissionRepository extends JpaRepository<Permission, UUID> {
+
+  @Query(
+      value =
+          "SELECT p FROM Permission p "
+              + "JOIN FETCH Sysconfig r ON p.role.id = r.id "
+              + "JOIN FETCH Sysconfig rp ON p.resourcePath.id = rp.id "
+              + "WHERE r.id = ?1 AND "
+              + "rp.value = ?2 AND "
+              + "p.deletedAt IS NULL AND "
+              + "r.deletedAt IS NULL AND "
+              + "rp.deletedAt IS NULL")
+  List<Permission> findAllActiveByRoleIdAndResourcePath(UUID roleId, String resourcePath);
+}
---
+++ src/main/java/com/fw/irongate/repositories/SysconfigRepository.java
@@ -0,0 +1,13 @@
+package com.fw.irongate.repositories;
+
+import com.fw.irongate.models.entities.Sysconfig;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SysconfigRepository extends JpaRepository<Sysconfig, UUID> {
+
+  @Query(value = "SELECT s FROM Sysconfig s WHERE s.id = ?1 AND s.deletedAt IS NULL")
+  Optional<Sysconfig> findOneActiveById(UUID id);
+}
