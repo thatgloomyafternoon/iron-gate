e7acedd0b15ffb99f5a785b678a36ea4879c5ba4
fw <thatgloomyafternoon@gmail.com>
2025-12-19 01:23:06 +0700

    progress

---
+++ src/main/java/com/fw/irongate/web/api/AuthController.java
@@ -0,0 +1,49 @@
+package com.fw.irongate.web.api;
+
+import static com.fw.irongate.constants.SystemConstants.COOKIE_NAME;
+import static com.fw.irongate.constants.SystemConstants.OK;
+
+import com.fw.irongate.models.dto.JwtClaimDTO;
+import com.fw.irongate.usecases.login.LoginUseCase;
+import com.fw.irongate.usecases.logout.LogoutUseCase;
+import com.fw.irongate.web.requests.LoginRequest;
+import com.fw.irongate.web.responses.MessageResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+  private final LoginUseCase loginUseCase;
+  private final LogoutUseCase logoutUseCase;
+
+  public AuthController(LoginUseCase loginUseCase, LogoutUseCase logoutUseCase) {
+    this.loginUseCase = loginUseCase;
+    this.logoutUseCase = logoutUseCase;
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<MessageResponse> login(
+      @RequestBody @Valid LoginRequest request, HttpServletResponse response) {
+    response.addHeader(HttpHeaders.SET_COOKIE, loginUseCase.handle(request).toString());
+    return ResponseEntity.ok(new MessageResponse(OK));
+  }
+
+  @PostMapping("/logout")
+  public ResponseEntity<MessageResponse> logout(
+      @AuthenticationPrincipal JwtClaimDTO jwtClaimDTO,
+      @CookieValue(name = COOKIE_NAME) String jwt,
+      HttpServletResponse response) {
+    response.addHeader(HttpHeaders.SET_COOKIE, logoutUseCase.handle(jwtClaimDTO, jwt).toString());
+    return ResponseEntity.ok(new MessageResponse((OK)));
+  }
+}
