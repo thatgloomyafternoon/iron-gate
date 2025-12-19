2e44bbd0d4dbda4a7c6f580fc6dd541a9d985689
fw <thatgloomyafternoon@gmail.com>
2025-12-19 11:52:19 +0700

    progress

--- src/test/java/com/fw/irongate/usecases/login/TestUserDetailsUseCase.java
+++ src/test/java/com/fw/irongate/usecases/login/TestUserDetailsUseCase.java
@@ -1,6 +1,9 @@
 package com.fw.irongate.usecases.login;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
