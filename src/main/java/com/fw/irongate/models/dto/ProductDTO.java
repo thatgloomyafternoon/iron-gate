1c57a855a22931ca1631eb89dfc72ab6ecda5d14
fw <thatgloomyafternoon@gmail.com>
2025-12-20 23:45:40 +0700

    progress

---
+++ src/main/java/com/fw/irongate/models/dto/ProductDTO.java
@@ -0,0 +1,7 @@
+package com.fw.irongate.models.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductDTO(
+    UUID id, String name, String sku, String description, BigDecimal price, Integer quantity) {}
