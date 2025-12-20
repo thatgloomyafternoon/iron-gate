6a4c513beff08b36adf5b94f9bb26768715d124c
fw <thatgloomyafternoon@gmail.com>
2025-12-21 00:04:02 +0700

    progress

--- src/main/java/com/fw/irongate/repositories/ProductRepository.java
+++ src/main/java/com/fw/irongate/repositories/ProductRepository.java
@@ -4,8 +4,10 @@ import com.fw.irongate.models.entities.Product;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository
+    extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 
   Optional<Product> findBySku(String sku);
 }
