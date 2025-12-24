0e7f354a7c073f2f4c96dacbc0d409f06269256f
fw <thatgloomyafternoon@gmail.com>
2025-12-24 15:41:48 +0700

    enable scheduling

--- src/main/java/com/fw/irongate/IronGate.java
+++ src/main/java/com/fw/irongate/IronGate.java
@@ -2,8 +2,10 @@ package com.fw.irongate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IronGate {
 
   public static void main(String[] args) {
