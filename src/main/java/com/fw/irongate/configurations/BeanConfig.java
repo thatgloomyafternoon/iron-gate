f4e33e5ab46ebd4ff013845fb9f5f4b916786002
fw <thatgloomyafternoon@gmail.com>
2025-12-24 15:41:09 +0700

    add TaskScheduler bean

--- src/main/java/com/fw/irongate/configurations/BeanConfig.java
+++ src/main/java/com/fw/irongate/configurations/BeanConfig.java
@@ -10,6 +10,8 @@ import java.security.SecureRandom;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -34,5 +36,14 @@ public class BeanConfig {
     return new ObjectMapper()
         .registerModule(new JavaTimeModule())
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+  }
+
+  @Bean
+  public TaskScheduler taskScheduler() {
+    ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+    threadPoolTaskScheduler.setPoolSize(10);
+    threadPoolTaskScheduler.setThreadNamePrefix("shipment-timer-");
+    threadPoolTaskScheduler.initialize();
+    return threadPoolTaskScheduler;
   }
 }
