277092aab698a64c96ce7f69b10c33b2d25f61c0
fw <thatgloomyafternoon@gmail.com>
2025-12-24 15:42:58 +0700

    add message constants

--- src/main/java/com/fw/irongate/constants/MessageConstants.java
+++ src/main/java/com/fw/irongate/constants/MessageConstants.java
@@ -7,5 +7,10 @@ public interface MessageConstants {
   String USER_ALREADY_LOGGED_OUT = "User \"{}\" already logged out";
   String SKU_ALREADY_EXISTS = "Product with the given SKU already exists";
   String OPERATION_NOT_PERMITTED = "Operation not permitted";
-  String PRODUCT_NOT_EXISTS = "Product not exists";
+  String PRODUCT_NOT_FOUND = "Product not found";
+  String STOCK_NOT_FOUND = "Stock not found";
+  String INSUFFICIENT_STOCK = "Insufficient stock to satisfy request";
+  String DEST_WAREHOUSE_NOT_FOUND = "Destination warehouse not found";
+  String SHIPMENT_NOT_FOUND = "Shipment not found";
+  String INVALID_STATE = "Invalid state";
 }
