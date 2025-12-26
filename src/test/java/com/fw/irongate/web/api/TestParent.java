3fd23540a0567aec65cf5ca0628f9065469d1ae6
fw <thatgloomyafternoon@gmail.com>
2025-12-26 18:51:52 +0700

    integration tests for /api/product/filter

--- src/test/java/com/fw/irongate/web/api/TestParent.java
+++ src/test/java/com/fw/irongate/web/api/TestParent.java
@@ -2,15 +2,18 @@ package com.fw.irongate.web.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fw.irongate.models.entities.Permission;
+import com.fw.irongate.models.entities.Product;
 import com.fw.irongate.models.entities.Sysconfig;
 import com.fw.irongate.models.entities.SysconfigType;
 import com.fw.irongate.models.entities.User;
 import com.fw.irongate.repositories.PermissionRepository;
+import com.fw.irongate.repositories.ProductRepository;
 import com.fw.irongate.repositories.RevokedTokenRepository;
 import com.fw.irongate.repositories.SysconfigRepository;
 import com.fw.irongate.repositories.SysconfigTypeRepository;
 import com.fw.irongate.repositories.UserRepository;
 import com.fw.irongate.utils.JwtUtil;
+import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +26,7 @@ public abstract class TestParent {
   @Autowired protected UserRepository userRepository;
   @Autowired protected PermissionRepository permissionRepository;
   @Autowired protected RevokedTokenRepository revokedTokenRepository;
+  @Autowired protected ProductRepository productRepository;
   @Autowired protected JwtUtil jwtUtil;
 
   @Autowired protected MockMvc mockMvc;
@@ -30,6 +34,7 @@ public abstract class TestParent {
   @Autowired protected ObjectMapper objectMapper;
 
   protected void deleteAll() {
+    productRepository.deleteAll();
     revokedTokenRepository.deleteAll();
     permissionRepository.deleteAll();
     userRepository.deleteAll();
@@ -37,33 +42,26 @@ public abstract class TestParent {
     sysconfigTypeRepository.deleteAll();
   }
 
-  protected SysconfigType createSysconfigType(
-      SysconfigTypeRepository repository, String name, String description) {
+  protected SysconfigType createSysconfigType(String name, String description) {
     SysconfigType st = new SysconfigType();
     st.setCreatedBy("system");
     st.setUpdatedBy("system");
     st.setName(name);
     st.setDescription(description);
-    return repository.save(st);
+    return sysconfigTypeRepository.save(st);
   }
 
-  protected Sysconfig createSysconfig(
-      SysconfigRepository repository, SysconfigType sysconfigType, String key, String value) {
+  protected Sysconfig createSysconfig(SysconfigType sysconfigType, String key, String value) {
     Sysconfig sysconfig = new Sysconfig();
     sysconfig.setCreatedBy("system");
     sysconfig.setUpdatedBy("system");
     sysconfig.setSysconfigType(sysconfigType);
     sysconfig.setKey(key);
     sysconfig.setValue(value);
-    return repository.save(sysconfig);
+    return sysconfigRepository.save(sysconfig);
   }
 
-  protected User createUser(
-      UserRepository repository,
-      Sysconfig role,
-      String email,
-      String passwordHash,
-      String fullName) {
+  protected User createUser(Sysconfig role, String email, String passwordHash, String fullName) {
     User user = new User();
     user.setCreatedBy("system");
     user.setUpdatedBy("system");
@@ -71,16 +69,26 @@ public abstract class TestParent {
     user.setEmail(email);
     user.setPasswordHash(passwordHash);
     user.setFullName(fullName);
-    return repository.save(user);
+    return userRepository.save(user);
   }
 
-  protected Permission createPermission(
-      PermissionRepository repository, Sysconfig role, Sysconfig resourcePath) {
+  protected Permission createPermission(Sysconfig role, Sysconfig resourcePath) {
     Permission permission = new Permission();
     permission.setCreatedBy("system");
     permission.setUpdatedBy("system");
     permission.setResourcePath(resourcePath);
     permission.setRole(role);
-    return repository.save(permission);
+    return permissionRepository.save(permission);
+  }
+
+  protected Product createProduct(String name, String sku, String description, BigDecimal price) {
+    Product product = new Product();
+    product.setCreatedBy("system");
+    product.setUpdatedBy("system");
+    product.setName(name);
+    product.setSku(sku);
+    product.setDescription(description);
+    product.setPrice(price);
+    return productRepository.save(product);
   }
 }
