59a9a7e24583a2a8ebb682f45cf4fe6c926db701
fw <thatgloomyafternoon@gmail.com>
2025-12-28 01:40:32 +0700

    update HQL in PermissionRepository to also retrieve the proxy instead of just fetching the relation data

--- src/main/java/com/fw/irongate/repositories/PermissionRepository.java
+++ src/main/java/com/fw/irongate/repositories/PermissionRepository.java
@@ -4,17 +4,19 @@ import com.fw.irongate.models.entities.Permission;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PermissionRepository extends JpaRepository<Permission, UUID> {
+public interface PermissionRepository
+    extends JpaRepository<Permission, UUID>, JpaSpecificationExecutor<Permission> {
 
   @Query(
       value =
           "SELECT p FROM Permission p "
-              + "JOIN FETCH Sysconfig r ON p.role.id = r.id "
-              + "JOIN FETCH Sysconfig rp ON p.resourcePath.id = rp.id "
-              + "JOIN FETCH SysconfigType rst ON r.sysconfigType.id = rst.id "
-              + "JOIN FETCH SysconfigType rpst ON rp.sysconfigType.id = rpst.id "
+              + "JOIN FETCH p.role r "
+              + "JOIN FETCH p.resourcePath rp "
+              + "JOIN FETCH r.sysconfigType rst "
+              + "JOIN FETCH rp.sysconfigType rpst "
               + "WHERE r.id = ?1 AND "
               + "rp.value = ?2 AND "
               + "p.deletedAt IS NULL AND "
