63530a1bd509c42256c3536670508c6369a3bd06
fw <thatgloomyafternoon@gmail.com>
2025-12-28 00:33:10 +0700

    add getter setter to Counter, integration tests for /api/shipment/create

--- src/main/java/com/fw/irongate/models/entities/Counter.java
+++ src/main/java/com/fw/irongate/models/entities/Counter.java
@@ -4,10 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("unused")
 @Table(name = "counters")
 @Entity
 public class Counter extends BaseEntity {
 
   @Column(name = "next", nullable = false)
   private Integer next;
+
+  public Integer getNext() {
+    return next;
+  }
+
+  public void setNext(Integer next) {
+    this.next = next;
+  }
 }
