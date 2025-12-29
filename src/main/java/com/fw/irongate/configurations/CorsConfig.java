27726295006d8ad1b967cae79331e3af18ac788e
fw <thatgloomyafternoon@gmail.com>
2025-12-30 04:55:58 +0700

    update cors config

--- src/main/java/com/fw/irongate/configurations/CorsConfig.java
+++ src/main/java/com/fw/irongate/configurations/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("http://localhost:5173", "https://gloomyafternoon.xyz")
             .allowedMethods(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
