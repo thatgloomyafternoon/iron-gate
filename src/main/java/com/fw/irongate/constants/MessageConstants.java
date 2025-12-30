7c340ac5966d246502cfcabaf161138d045d76e6
fw <thatgloomyafternoon@gmail.com>
2025-12-30 15:46:25 +0700

    fixing error handling

--- src/main/java/com/fw/irongate/constants/MessageConstants.java
+++ src/main/java/com/fw/irongate/constants/MessageConstants.java
@@ -2,7 +2,6 @@ package com.fw.irongate.constants;
 
 public interface MessageConstants {
 
-  String INVALID_JWT = "Invalid JWT";
   String USER_EMAIL_NOT_FOUND = "User email not found";
   String USER_ALREADY_LOGGED_OUT = "User \"{}\" already logged out";
   String SKU_ALREADY_EXISTS = "Product with the given SKU already exists";
