80e18dd3256aaf36d9dc196424a607a10aa3211a
fw <thatgloomyafternoon@gmail.com>
2026-01-01 14:58:28 +0700

    add STOCK_CREATED SSE event

--- src/main/java/com/fw/irongate/usecases/create_stock/CreateStockUseCase.java
+++ src/main/java/com/fw/irongate/usecases/create_stock/CreateStockUseCase.java
@@ -2,7 +2,9 @@ package com.fw.irongate.usecases.create_stock;
 
 import static com.fw.irongate.constants.MessageConstants.OPERATION_NOT_PERMITTED;
 import static com.fw.irongate.constants.MessageConstants.PRODUCT_NOT_FOUND;
+import static com.fw.irongate.constants.SystemConstants.EVENT_STOCK_CREATED;
 
+import com.fw.irongate.models.dto.DashboardEventDTO;
 import com.fw.irongate.models.dto.JwtClaimDTO;
 import com.fw.irongate.models.entities.Product;
 import com.fw.irongate.models.entities.Stock;
@@ -11,6 +13,7 @@ import com.fw.irongate.repositories.ProductRepository;
 import com.fw.irongate.repositories.StockRepository;
 import com.fw.irongate.repositories.WarehouseUserRepository;
 import com.fw.irongate.usecases.UseCase;
+import com.fw.irongate.usecases.stream_dashboard.StreamDashboardUseCase;
 import com.fw.irongate.web.responses.IdResponse;
 import java.util.List;
 import java.util.Optional;
@@ -21,14 +24,17 @@ public class CreateStockUseCase {
   private final WarehouseUserRepository warehouseUserRepository;
   private final ProductRepository productRepository;
   private final StockRepository stockRepository;
+  private final StreamDashboardUseCase streamDashboardUseCase;
 
   public CreateStockUseCase(
       WarehouseUserRepository warehouseUserRepository,
       ProductRepository productRepository,
-      StockRepository stockRepository) {
+      StockRepository stockRepository,
+      StreamDashboardUseCase streamDashboardUseCase) {
     this.warehouseUserRepository = warehouseUserRepository;
     this.productRepository = productRepository;
     this.stockRepository = stockRepository;
+    this.streamDashboardUseCase = streamDashboardUseCase;
   }
 
   public IdResponse handle(JwtClaimDTO jwtClaimDTO, CreateStockRequest request) {
@@ -69,6 +75,8 @@ public class CreateStockUseCase {
       stock.setQuantity(request.quantity().intValue());
       stock = stockRepository.save(stock);
     }
+    /* send event to frontend */
+    streamDashboardUseCase.broadcast(new DashboardEventDTO(EVENT_STOCK_CREATED));
     return new IdResponse(stock.getId());
   }
 }
